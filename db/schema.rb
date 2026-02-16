a1a112dd20a1919ebb64adbbf927c2b45de5a7d4
Varin Kangpanich <varin.k@rmagroup.net>
2026-02-16 15:41:52 +0700

    micreate add password_digest บน odoo Table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2026_01_14_201315) do
+ActiveRecord::Schema[7.1].define(version: 2026_02_16_153900) do
   # These extensions should be enabled to support this database
   enable_extension "pg_stat_statements"
   enable_extension "pg_trgm"
@@ -1058,6 +1058,7 @@ ActiveRecord::Schema[7.1].define(version: 2026_01_14_201315) do
     t.string "odoo_url"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "password_digest"
     t.index ["account_id"], name: "index_odoos_on_account_id"
   end
 
